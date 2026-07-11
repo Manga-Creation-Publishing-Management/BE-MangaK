200423651b5b24171dad596774ff57ee2cce3bfa
Tuấn Lê <tuanle17082k5@gmail.com>
2026-07-11 20:33:18 +0700

    sửa app.UseSwagger();

--- Manga.Api/Program.cs
+++ Manga.Api/Program.cs
@@ -97,8 +97,7 @@ using (var scope = app.Services.CreateScope())
     await AppDbContextSeed.SeedAsync(db);
 }
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwaggerAPI();
 //AI
 app.UseCors("AllowFrontend");
 //AI
